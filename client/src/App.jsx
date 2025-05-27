0c52afdd142b2d57c9d2edf7d34f95ffc80d97af
mraolvr <austinblakeoliver@gmail.com>
2025-05-27 13:02:09 -0400

    fix: remove nested Router to resolve routing error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { UNSAFE_NavigationContext as NavigationContext } from 'react-router-dom';
 import Sidebar, { SidebarBody, SidebarLink } from './components/layout/Sidebar';
 import Dashboard from './pages/dashboard/Dashboard';
@@ -31,81 +31,79 @@ function App() {
     <ThemeProvider>
       <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
         <UserProvider>
-          <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-            <Toaster position="top-right" />
-            <div className="min-h-screen text-white bg-gradient-to-br from-violet-600 via-fuchsia-500 via-40% to-emerald-500 dark:bg-gradient-to-br dark:from-violet-950 dark:via-fuchsia-900 dark:via-40% dark:to-emerald-900">
-              <div className="relative min-h-screen">
-                {/* Main content */}
-                <div className="flex min-h-screen">
-                  <SidebarBody>
-                    {routes.map((route) => (
-                      <SidebarLink
-                        key={route.path}
-                        to={route.path}
-                        icon={route.icon}
-                      >
-                        {route.name}
-                      </SidebarLink>
-                    ))}
-                    <div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent my-4" />
-                    {bottomRoutes.map((route) => (
-                      <SidebarLink
-                        key={route.path}
-                        to={route.path}
-                        icon={route.icon}
-                      >
-                        {route.name}
-                      </SidebarLink>
-                    ))}
-                  </SidebarBody>
-                  <div className="flex-1 md:ml-[280px]">
-                    <Header />
-                    <main className="p-6 md:p-8 lg:p-12">
-                      <div className="max-w-[1600px] mx-auto">
-                        <Routes>
-                          <Route path="/" element={
-                            <ProtectedRoute>
-                              <Dashboard />
-                            </ProtectedRoute>
-                          } />
-                          <Route path="/guidance" element={
-                            <ProtectedRoute>
-                              <GuidanceHub />
-                            </ProtectedRoute>
-                          } />
-                          <Route path="/work" element={
-                            <ProtectedRoute>
-                              <WorkHub />
-                            </ProtectedRoute>
-                          } />
-                          <Route path="/books" element={<ProtectedRoute><BookLibrary /></ProtectedRoute>} />
-                          <Route path="/creative" element={<ProtectedRoute><CreativeHub /></ProtectedRoute>} />
-                          <Route path="/vault" element={<ProtectedRoute><Vault /></ProtectedRoute>} />
-                          <Route path="/calendar" element={
-                            <ProtectedRoute>
-                              <CalendarPage />
-                            </ProtectedRoute>
-                          } />
-                          <Route path="/contacts" element={<ProtectedRoute><Contacts /></ProtectedRoute>} />
-                          <Route path="/settings" element={
-                            <ProtectedRoute>
-                              <SettingsPage />
-                            </ProtectedRoute>
-                          } />
-                          <Route path="/notifications" element={<ProtectedRoute><NotificationsPage /></ProtectedRoute>} />
-                          <Route path="/help" element={<ProtectedRoute><HelpPage /></ProtectedRoute>} />
-                          <Route path="/logout" element={<ProtectedRoute><LogoutPage /></ProtectedRoute>} />
-                          <Route path="/login" element={<Login />} />
-                          <Route path="/auth/callback" element={<AuthCallback />} />
-                          <Route path="*" element={<ProtectedRoute><NotFoundPage /></ProtectedRoute>} />
-                        </Routes>
-                      </div>
-                    </main>
-                  </div>
+          <Toaster position="top-right" />
+          <div className="min-h-screen text-white bg-gradient-to-br from-violet-600 via-fuchsia-500 via-40% to-emerald-500 dark:bg-gradient-to-br dark:from-violet-950 dark:via-fuchsia-900 dark:via-40% dark:to-emerald-900">
+            <div className="relative min-h-screen">
+              {/* Main content */}
+              <div className="flex min-h-screen">
+                <SidebarBody>
+                  {routes.map((route) => (
+                    <SidebarLink
+                      key={route.path}
+                      to={route.path}
+                      icon={route.icon}
+                    >
+                      {route.name}
+                    </SidebarLink>
+                  ))}
+                  <div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent my-4" />
+                  {bottomRoutes.map((route) => (
+                    <SidebarLink
+                      key={route.path}
+                      to={route.path}
+                      icon={route.icon}
+                    >
+                      {route.name}
+                    </SidebarLink>
+                  ))}
+                </SidebarBody>
+                <div className="flex-1 md:ml-[280px]">
+                  <Header />
+                  <main className="p-6 md:p-8 lg:p-12">
+                    <div className="max-w-[1600px] mx-auto">
+                      <Routes>
+                        <Route path="/" element={
+                          <ProtectedRoute>
+                            <Dashboard />
+                          </ProtectedRoute>
+                        } />
+                        <Route path="/guidance" element={
+                          <ProtectedRoute>
+                            <GuidanceHub />
+                          </ProtectedRoute>
+                        } />
+                        <Route path="/work" element={
+                          <ProtectedRoute>
+                            <WorkHub />
+                          </ProtectedRoute>
+                        } />
+                        <Route path="/books" element={<ProtectedRoute><BookLibrary /></ProtectedRoute>} />
+                        <Route path="/creative" element={<ProtectedRoute><CreativeHub /></ProtectedRoute>} />
+                        <Route path="/vault" element={<ProtectedRoute><Vault /></ProtectedRoute>} />
+                        <Route path="/calendar" element={
+                          <ProtectedRoute>
+                            <CalendarPage />
+                          </ProtectedRoute>
+                        } />
+                        <Route path="/contacts" element={<ProtectedRoute><Contacts /></ProtectedRoute>} />
+                        <Route path="/settings" element={
+                          <ProtectedRoute>
+                            <SettingsPage />
+                          </ProtectedRoute>
+                        } />
+                        <Route path="/notifications" element={<ProtectedRoute><NotificationsPage /></ProtectedRoute>} />
+                        <Route path="/help" element={<ProtectedRoute><HelpPage /></ProtectedRoute>} />
+                        <Route path="/logout" element={<ProtectedRoute><LogoutPage /></ProtectedRoute>} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/auth/callback" element={<AuthCallback />} />
+                        <Route path="*" element={<ProtectedRoute><NotFoundPage /></ProtectedRoute>} />
+                      </Routes>
+                    </div>
+                  </main>
                 </div>
               </div>
             </div>
-          </Router>
+          </div>
         </UserProvider>
       </GoogleOAuthProvider>
     </ThemeProvider>
